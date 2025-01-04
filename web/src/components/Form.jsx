e04f32b01c65a48d2a80476eae03998e620a9da9
d4julian <julianubico@gmail.com>
2025-01-04 15:05:10 -0500

    Use dummy response

--- web/src/components/Form.jsx
+++ web/src/components/Form.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import Button from "./Button";
 import useCategorize from "../hooks/useCategorize";
 
 export default function Form({ onCategorizeSuccess }) {
